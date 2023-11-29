5662f07b30e218928f0795f6502157cbe7d15e28
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-29 10:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const list = document.querySelector('#list');
+const list = document.querySelector('#band');
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 let result = [];
 bands.forEach(elem => {
