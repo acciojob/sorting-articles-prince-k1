6ff7fdf0d819e90d01b9df8f2380d01c6008bbf3
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-29 10:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const list = document.querySelector('#list');
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+let result = [];
+bands.forEach(elem => {
+	let str = elem.split(' ');
+	// console.log(str);
+	let  done = str.filter(e => {
+		let s = e.toLowerCase();
+		// console.log(s);
+		 if(s != 'the' && s != 'a' && s != 'an'){
+			// console.log(e);
+			return e;
+		 }
+	})
+	
+	result.push(done.join(' '));
+})
+result.sort();
+let final = [];
+for(let i = 0; i < result.length; i++){
+	for(let j = 0; j < result.length; j++){
+		if(bands[j].includes(result[i])){
+			final.push(bands[j]);
+		}
+	}
+}
+
+final.forEach(elem => {
+	let li = document.createElement('li');
+	li.innerText = elem;
+	list.appendChild(li);
+})
